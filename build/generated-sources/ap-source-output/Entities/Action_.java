fca9a34420a28568331aae864a8664934d03d257
Ahmed Shuman <Ahmed.shuman99@gmail.com>
2024-03-22 06:07:58 +0200

    n

--- build/generated-sources/ap-source-output/Entities/Action_.java
+++ build/generated-sources/ap-source-output/Entities/Action_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-22T00:54:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-22T05:45:48")
 @StaticMetamodel(Action.class)
 public class Action_ { 
 
--- build/generated-sources/ap-source-output/Entities/Product_.java
+++ build/generated-sources/ap-source-output/Entities/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-22T04:35:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-22T05:45:48")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
